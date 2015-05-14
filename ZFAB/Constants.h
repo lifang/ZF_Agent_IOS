9e7750e5ecd4fd725ffa368ec8f07cafe60aa69b
XuBingoo <251311016@qq.com>
2015-05-14 21:15:59 +0800

    5.14

--- ZFAB/Constants.h
+++ ZFAB/Constants.h
@@ -24,8 +24,8 @@
 
 #define kCoolDownTime  120   //冷却时间
 
-#define kServiceURL @"http://121.40.64.167:9090/api"
-//#define kServiceURL @"http://121.40.84.2:28080/ZFAgent/api"
+//#define kServiceURL @"http://121.40.64.167:9090/api"
+#define kServiceURL @"http://121.40.84.2:28080/ZFAgent/api"
 //#define kServiceURL @"http://agent.ebank007.com/api"
 
 //#define kVideoAuthIP    @"121.40.84.2"
